3b9b9789533e595b6e03a15cc0c8b4e558bb845d
Valentine Omonya <valomosh254@gmail.com>
2025-09-07 23:19:42 +0300

    Initiated a go dns server with net http created dns reader and writer

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
+// var _ = net.ListenUDP
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	// fmt.Println("Logs from your program will appear here!")
+	Test()
+	// Uncomment this block to pass the first stage
+	//
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+	fmt.Println("Server listening to UDP connection on ", udpAddr)
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer udpConn.Close()
+
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			// break
+		}
+
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		response := []byte{byte(4), byte(210), byte(128), 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
+}
